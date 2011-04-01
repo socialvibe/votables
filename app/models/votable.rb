f46547facdf69c206e1eda83a0e5cb57573e6ca6
rangsikitpho <rangsikitpho@gmail.com>
2011-04-01 16:11:43 -0700

    isowner

--- app/models/votable.rb
+++ app/models/votable.rb
@@ -1,11 +1,16 @@
 class Votable < ActiveRecord::Base
   has_many :rounds, :dependent => :destroy
   has_many :options, :dependent => :destroy
+  belongs_to :user
 
   accepts_nested_attributes_for :rounds, :allow_destroy => true
   accepts_nested_attributes_for :options, :allow_destroy => true
 
   def num_voters
     5
+  end
+
+  def is_owner?(u)
+    user == u
   end
 end
