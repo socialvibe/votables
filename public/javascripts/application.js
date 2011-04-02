335a24308ec2aadef2efcaf7500eeaee13c7f260
A N <aslan@ans-macbook-pro.westell.com>
2011-04-01 18:37:09 -0700

    name field in registration

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function () {
+	$("#user_new").validate({
+		debug: true,
+		rules: {
+			"user[name]": {required: true, minlength: 2},
+			// "user[password]": {required: true, minlength: 6},
+			// "user[password_confirmation]": {required: true, equalTo: "#user_password"}
+		}
+	});
+});
